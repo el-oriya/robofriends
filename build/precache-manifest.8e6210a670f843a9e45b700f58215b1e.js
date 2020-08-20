b118d6eb4e9c1e7d1c9c92e2b4eea8ddd507bbff
the-solutionist <emmanuelattiah64@gmail.com>
2020-08-20 21:08:31 +0000

    it failed

---
+++ build/precache-manifest.8e6210a670f843a9e45b700f58215b1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6472e19e7ec7b6d8512cc1662a8b226e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2cdf63f1aba7b55f50d3",
+    "url": "/static/css/main.7b41de6e.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cdf63f1aba7b55f50d3",
+    "url": "/static/js/main.47e620a4.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
